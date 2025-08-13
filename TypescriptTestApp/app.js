5ac3937fdd6bf7a09c6c5a2a57b631537d36bd1d
Seth-Ewer <seth@3w3r.com>
2025-08-13 18:46:02 -0400

    Added mergeKLists function and mergeKListsRecursion helper function. Added ListNodeConstructor to speed up creating ListNodes

--- TypescriptTestApp/app.js
+++ TypescriptTestApp/app.js
@@ -3,18 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class ListNode {
     val;
     next;
+    v = false;
     constructor(val, next) {
         this.val = (val === undefined ? 0 : val);
         this.next = (next === undefined ? null : next);
     }
 }
-class ListNodePlusId {
-    id;
-    value;
-    constructor(newId, newVal) {
-        this.id = newId;
-        this.value = newVal;
-    }
+function ListNodeConstructor(valArr) {
+    var val = valArr[0] ? (valArr.shift()) : 0;
+    var next = valArr.length > 0 ? ListNodeConstructor(valArr) : null;
+    return new ListNode(val, next);
 }
 function reverseList(head) {
     if (head) {
@@ -65,12 +63,64 @@ function mergeTwoLists(list1, list2) {
     }
 }
 ;
+function mergeKListsRecursion(lists, list) {
+    if (list && lists[0]) {
+        if (list.val <= lists[0].val) {
+            list.next = mergeKListsRecursion(lists, list.next);
+            return list;
+        }
+        else {
+            var newFirstList = lists.shift();
+            var newIndex = lists.findIndex(a => a.val > list.val);
+            if (newIndex == -1) {
+                lists.push(list);
+            }
+            else {
+                lists.splice(newIndex, 0, list);
+            }
+            newFirstList.next = mergeKListsRecursion(lists, newFirstList.next);
+            return newFirstList;
+        }
+    }
+    else if (lists[0]) {
+        var temp = lists.shift();
+        if (temp) {
+            temp.next = mergeKListsRecursion(lists, temp.next);
+            return temp;
+        }
+        else {
+            return null;
+        }
+    }
+    else {
+        return list;
+    }
+}
+function mergeKLists(lists) {
+    if (lists) {
+        lists = lists.filter(a => a);
+        var temp = lists.shift();
+        if (temp) {
+            temp.next = mergeKListsRecursion(lists, temp.next);
+            return temp;
+        }
+        else {
+            return null;
+        }
+    }
+    else {
+        return null;
+    }
+}
+;
 var node = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, null)))));
 //reverseList(node);
 var point = new ListNode(3, new ListNode(2, new ListNode(0, new ListNode(-4, null))));
 point.next.next.next = point.next;
-hasCycle(point);
+//hasCycle(point);
 var lOne = new ListNode(1, new ListNode(2, new ListNode(4, null)));
 var lTwo = new ListNode(1, new ListNode(3, new ListNode(4, null)));
 //mergeTwoLists(lOne, lTwo);
+var MergeKListsTest = [ListNodeConstructor([1, 4, 5]), ListNodeConstructor([1, 3, 4]), ListNodeConstructor([2, 6])];
+mergeKLists(MergeKListsTest);
 //# sourceMappingURL=app.js.map
--- TypescriptTestApp/app.ts
+++ TypescriptTestApp/app.ts
@@ -2,20 +2,16 @@ class ListNode {
       val: number
   next: ListNode | null
   v: boolean | null = false
-      constructor(val?: number, next?: ListNode | null) {
+  constructor(val?: number, next?: ListNode | null) {
  this.val = (val === undefined ? 0 : val)
       this.next = (next === undefined ? null : next)
-      }
- }
-
-class ListNodePlusId{
-  id: number
-  value: ListNode
-  constructor(newId: number, newVal: ListNode)
-  {
-    this.id = newId;
-    this.value = newVal;
   }
+}
+function ListNodeConstructor(valArr: Array<number>): ListNode | null
+{
+  var val = valArr[0] ? (valArr.shift())! : 0;
+  var next = valArr.length > 0 ? ListNodeConstructor(valArr) : null;
+  return new ListNode(val, next);
 }
 
 function reverseList(head: ListNode | null): ListNode | null {
@@ -67,6 +63,50 @@ function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode
 
 
 
+function mergeKListsRecursion(lists: Array<ListNode | null>, list: ListNode | null): ListNode | null {
+  if (list && lists[0]) {
+    if (list.val <= lists[0].val) {
+      list.next = mergeKListsRecursion(lists, list.next);
+      return list;
+
+    } else
+    {
+      var newFirstList = lists.shift()!;
+      var newIndex = lists.findIndex(a => a!.val > list.val);
+      if (newIndex == -1) {
+        lists.push(list);
+      } else {
+        lists.splice(newIndex, 0, list);
+      }
+      newFirstList.next = mergeKListsRecursion(lists, newFirstList.next);
+      return newFirstList;
+    }
+
+  }
+  else if (lists[0]) {
+    var temp = lists.shift();
+    if (temp) {
+      temp.next = mergeKListsRecursion(lists, temp.next);
+      return temp;
+    } else { return null; }
+  } else {
+    return list;
+  }
+}
+function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
+
+  if (lists) {
+    lists = lists.filter(a => a);
+    lists.sort((a,b) => a!.val-b!.val);
+    var temp = lists.shift();
+    if (temp) {
+      temp.next = mergeKListsRecursion(lists, temp.next);
+      return temp;
+    } else { return null; }
+  } else { return null; }
+};
+
+
 var node = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, null)))));
 
 //reverseList(node);
@@ -75,7 +115,7 @@ var node = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new L
 var point = new ListNode(3, new ListNode(2, new ListNode(0, new ListNode(-4, null))));
 
 point!.next!.next!.next = point.next;
-hasCycle(point);
+//hasCycle(point);
 
 
 var lOne = new ListNode(1, new ListNode(2, new ListNode(4, null)));
@@ -83,3 +123,8 @@ var lOne = new ListNode(1, new ListNode(2, new ListNode(4, null)));
 var lTwo = new ListNode(1, new ListNode(3, new ListNode(4, null)));
 
 //mergeTwoLists(lOne, lTwo);
+
+
+var MergeKListsTest = [ListNodeConstructor([1, 4, 5]), ListNodeConstructor([1, 3, 4]), ListNodeConstructor([2, 6])];
+
+mergeKLists(MergeKListsTest);
